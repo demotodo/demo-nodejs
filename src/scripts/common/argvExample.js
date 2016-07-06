024a57c85f6293a1b622f8444df7533b2d6e1856
bribin <bribin@163.com>
2016-07-06 23:09:28 +0800

    examples from book

---
+++ src/scripts/common/argvExample.js
@@ -0,0 +1,5 @@
+console.log('hello world');
+
+for (var i = 0, length = process.argv.length; i < length; i++) {
+    console.log(process.argv[i]);
+}
--- src/scripts/http/httpExample.js
+++ src/scripts/http/httpExample.js
@@ -1,13 +1,7 @@
-console.log('hello world');
-
-for (var i = 0, length = process.argv.length; i < length; i++) {
-    console.log(process.argv[i]);
-}
-
 var http = require("http"),
     server,
     HTTP_OK = 200,
-    PORT = 8000;
+    PORT = 3000;
 
 server = http.createServer(function (request, response) {
     response.writeHead(HTTP_OK, {
